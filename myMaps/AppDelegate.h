64f3593816d8eff962f7b2241b33ce4c75d4d2e8
PiHan Hsu <pihanhsu@gmail.comgit config --global user.name PiHanHsu>
2014-11-04 18:28:12 +0800

    Initial Commit

---
+++ myMaps/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myMaps
+//
+//  Created by PiHan Hsu on 2014/11/4.
+//  Copyright (c) 2014年 PiHan Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myMaps/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myMaps
+//
+//  Created by PiHan Hsu on 2014/11/4.
+//  Copyright (c) 2014年 PiHan Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
